38c1390c5fba6c0157da1d82bc3b8ae80ac78e29
Vinicius Brunheroto <vinibrunheroto12@gmail.com>
2026-02-01 20:53:43 -0300

    fix: refund link

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -158,7 +158,7 @@ export function Refund() {
 
       {params.id && fileURL ? (
         <a
-          href={`http://localhost:3333/uploads/${fileURL}`}
+          href={`${import.meta.env.VITE_API_URL}/uploads/${fileURL}`}
           target="_blank"
           className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6
           hover:opacity-70 transition ease-linear"
